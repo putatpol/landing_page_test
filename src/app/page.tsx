668ce72dce2b1903794311fa18fcd8f0f5a474dc
maxgatopv <maxga.p@topvalue.com>
2025-10-14 18:43:47 +0700

    add color and font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import { outfitFont, notoSansJpFont, adobeFonts } from "../styles/fonts";
 export default function Home() {
   return (
     <>
-      <h1 className={outfitFont.className}>outfit</h1>
-      <h2 className={notoSansJpFont.className}>すべての</h2>
-      <h1 className={adobeFonts.mizolet}>Hello Mizolet!</h1>
+      <h1 className={`${outfitFont.className} text-custom-yellow`}>outfit</h1>
+      <h2 className={`${notoSansJpFont.className} text-custom-red`}>すべての</h2>
+      <h1 className={`${adobeFonts.mizolet} text-custom-green-dark`}>Hello Mizolet!</h1>
       <p className={adobeFonts.yuGothic}>これは Yu Gothic フォントです</p>
     </>
   );
